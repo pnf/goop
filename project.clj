7aeda219e73f4fd2c3ec6019b2f46c6c3d9e40c6
Peter Fraenkel <pnf@podsnap.com>
2015-11-01 13:01:27 -0500

    Upgrade async

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.async "0.2.371"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  ])
