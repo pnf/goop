2cc90d105c95bec41308c9556ebca625e2f0d9d8
Peter Fraenkel <pnf@podsnap.com>
2016-09-17 15:13:17 -0400

    Rewrite as multimethod.

--- src/goop/core.clj
+++ src/goop/core.clj
@@ -15,16 +15,6 @@
 ;;(defn- await-chan [ch] `(a/<! ~ch))
 (defn await-chan [ch] `(deref ~ch))
 (defmacro <! [ch] (await-chan ch))
-(defmacro go [form] (form-to-chan form))
-
-
-(defn build-let [forms bs]
-  (cond (seq bs)
-        `(let [~@bs] ~@forms)
-        (> (count forms) 1)
-        `(do ~@forms)
-        :else
-        (first forms)))
 
 (def cache (atom (soft-cache-factory {})))
 
@@ -39,11 +29,13 @@
         (swap! cache assoc (conj args f) ret)
         ret))))
 
-(declare parallelize parallelize-goop-call parallelize-function-call)
+(declare parallelize)
 
+;; Present as a normal function, except that gfn is a parallelized promise.
 (defrecord GoopFn [gfn]
   clojure.lang.Fn
   clojure.lang.IFn
+  ;; Hack, since you can't have variadic implementations.
   (invoke [this x1] (<! (gfn x1)))
   (invoke [this x1 x2] (<! (gfn x1 x2)))
   (invoke [this x1 x2 x3] (<! (gfn x1 x2 x3)))
@@ -51,7 +43,10 @@
   (invoke [this x1 x2 x3 x4 x5] (<! (gfn x1 x2 x3 x4 x5)))
   (applyTo [this args] (clojure.lang.AFn/applyToHelper this args)))
 
-(defmacro gdefn [fname args body]
+(defmacro gdefn
+  "Like defn, but defines a goop function with a parallelized body.
+Sadly, doesn't deal with doc strings and metadata."
+  [fname args body]
   (let [{:keys [form ch bs]} (parallelize body {} "goop-")
         cdef (if ch
                `(let [~@bs] ch)
@@ -59,7 +54,10 @@
         gfn `(soft-memoize (q/sfn ~args ~cdef))]
     `(def ~fname (->GoopFn ~gfn))))
 
-(defmacro gfn [args body]
+(defmacro gfn
+  "Like fn, but defines a goop function with a parallelized body.
+Sadly, doesn't deal with doc strings and metadata."
+  [args body]
   (let [{:keys [form ch bs]} (parallelize body {} "goop-")
         cdef (if ch
                `(let [~@bs] ch)
@@ -67,48 +65,90 @@
         gfn `(soft-memoize (q/sfn ~args ~cdef))]
     `(->GoopFn ~gfn)))
 
-(defmacro goop [form]
+(defmacro goop
+  "Parallelize and evaluate an expression"
+  [form]
   (let [{:keys [form ch bs]} (parallelize form {} "goop-")]
     (let [res (if ch
                 `(let [~@bs] ~(await-chan ch))
                 form)]
-      (pprint res)
+      ;(pprint res)
       res)))
 
-(defn goop-fn [fs]
+(defn goop-fn
+  "If the symbol represents a goop function, returns its gfn, otherwise nil."
+  [fs]
   (when (or (and (symbol? fs) (some-> fs resolve var-get :gfn))
           (and (list? fs) (= (first fs) 'gfn)))
     `(.gfn ~fs)))
 
-(defn as-goop-fn [fs]
+(defn as-goop-fn
+  "Given a symbol representing a function, make it a goop function if it's not already."
+  [fs]
   (or (goop-fn fs)
       `(gfn [& args#] (apply ~fs args#))))
 
+(defn- res= [a b] (= (resolve a) (resolve b)))
 
-(defn parallelize-coll [form sym->chan ch-prefix]
-  (println "parallelize coll" form)
-  (let [ps (map #(parallelize % sym->chan ch-prefix) (seq form))
+(defmulti parallelize
+  "Parallelize a form, returning map:
+    :form      Parallelized form that can be substituted.
+    :bs        Bindings that defines the channel
+    :ch        Channel to be dereferenced
+    :par       parallel?
+    :s2c       Map of user symbols to channel symbols
+  To evaluate, one would apply the bindings and then dereference the channel."
+  (fn [form & args]
+    (cond
+      (nil? args)              :defaults
+      (seq? form)
+      (if-let [f (first form)]
+        (cond (res= 'let f)    :let
+              (res= 'map f)    :map
+              (or (function? f)
+                  (goop-fn f)) :func
+              :else            :coll)
+                               :coll)
+      (coll? form)             :coll
+      :else                    :subst)))
+
+
+;; Add default symbol->channel map and channel prefix.
+(defmethod parallelize :defaults [form] (parallelize form {} "ch"))
+
+;; If there's a channel that will return the value of this symbol, substitute its deref.
+(defmethod parallelize :subst [form s2c chp]
+  (if-let [ch (get s2c form)]
+    {:ch ch :form (await-chan ch) :s2c s2c :par true}
+    {:form form}))
+
+;; Parallelize a collection.
+(defmethod parallelize :coll [form s2c chp]
+  (let [;;   Recursively parallelize members.
+        ps   (map #(parallelize % s2c chp) (seq form))
+        ;;   Extract bindings and transformed members
         bs   (mapcat :bs ps)
         args (map :form ps)
         par  (some? (some :par ps))]
-    (if par
-      (let [ch    (gensym ch-prefix)
+    (if-not par
+      {:form form}
+      (let [ch    (gensym chp)
+            ;;    Create parallelized collection of same type as original.
             rhs   (cond (vector? form) `[~@args]
                         (map? form)    `(hash-map ~@args)
                         (set? form)    `#{~@args}
                         :else          `(list ~@args))
+            ;;    Bind the new collection to the new channel.
             bs    (concat bs [ch (form-to-chan rhs)])
             form  (await-chan ch)]
-        {:form form :par true :bs bs :ch ch})
-      {:form form})))
+        {:form form :par true :bs bs :ch ch}))))
 
-
-(defn parallelize-function-call [[f & args] sym->chan ch-prefix]
-  (println "parallelize-function-call" f args)
-  (let [{args :form bs :bs
-         par  :par} (parallelize-coll args sym->chan ch-prefix)
+;; Parallelize a function call
+(defmethod parallelize :func [[f & args] s2c chp]
+  (let [;; Parallelize the arguments.
+        {args :form bs :bs par :par} ((get-method parallelize :coll) args s2c chp)
         gfn  (goop-fn f)
-        ch   (gensym ch-prefix)]
+        ch   (gensym chp)]
     (cond (and par gfn) ;; goop call with par args
           {:ch ch :form (await-chan ch)  :par true
            :bs (concat bs [ch `(apply ~gfn ~args)])}
@@ -122,74 +162,48 @@
           {:form `(~f ~@args)})))
 
 (defn map-and-deref [g & args] (map (fn [c] (<! c)) (apply map g args)))
-(defn parallelize-map [[_ f & args] sym->chan ch-prefix]
-  (println "parallelize-map" args (type args))
+(defmethod parallelize :map [[_ f & args] s2c chp]
   (let [gfn   (goop-fn f)
         mf (if gfn ['map-and-deref gfn] ['map f])]
-    (parallelize-function-call (concat mf args) sym->chan ch-prefix)))
+    (parallelize-function-call (concat mf args) s2c chp)))
 
-(defn parallelize-let [[_ bs & forms] sym->chan ch-prefix]
-  (let [;; Parallelize rhs of each binding, accruing sym->chan map.
-        {sym->chan :sym->chan bs :bs}  (reduce (fn [{sym->chan0 :sym->chan bs0 :bs} [a v]]
-                                                 (println "let loopy" sym->chan0 bs0 a v)
-                                                 (let [{:keys [form sym->chan par bs ch]} (parallelize v sym->chan0 a)]
-                                                   (println "let loop" a v par bs ch sym->chan0 sym->chan)
-                                                   (if ch
-                                                     {:sym->chan (assoc sym->chan0 a ch) :bs (concat bs0 bs)}
-                                                     {:sym->chan sym->chan0              :bs (concat bs0 [a v])})))
-                                               {:sym->chan sym->chan :bs []}
-                                               (partition 2 bs))
-        ;; Parallelize the forms
-        ps (map #(parallelize % sym->chan "form-") forms)
+;; Incorporate a new let binding.
+(defn- accrue-bindings [{s2c0 :s2c bs0 :bs} [a v]]
+  (let [{:keys [form s2c par bs ch]} (parallelize v s2c0 a)]
+    (if ch
+      ;; Record the channel to deref to get the value of the symbol, and the binding to
+      ;; define the channel.
+      {:s2c (assoc s2c0 a ch) :bs (concat bs0 bs)}
+      ;; Non-parallel term.  Just add the plain binding.
+      {:s2c s2c0              :bs (concat bs0 [a v])})))
+
+(defn- build-let
+  "Create a full let form from bindings and contained forms."
+  [forms bs]
+  (cond (seq bs) ;; Actually have bindings.
+        `(let [~@bs] ~@forms)
+        (> (count forms) 1) ;; No bindings but multiple forms.
+        `(do ~@forms)
+        :else               ;; Just one form.
+        (first forms)))
+
+(defmethod parallelize :let [[_ bs & forms] s2c chp]
+  (let [{s2c :s2c bs :bs} (reduce accrue-bindings {:s2c s2c :bs []} (partition 2 bs))
+        ps (map #(parallelize % s2c "let-form") forms)
+        ;; If there were parallel bindings, but not forms, then someone is launching channels
+        ;; unnecessarily, but no matter.
         par (some :par ps)
-        ;; Augment bindings with channel definitions for each form
         bs (apply concat bs (map :bs ps))
         forms (map :form ps)]
     (if par
-      (let [ch (gensym ch-prefix)
+      (let [ch (gensym chp)
             cdef  (form-to-chan (build-let forms bs))]
-        {:par true
-         :ch ch
-         :bs [ch cdef]
-         :form (await-chan ch)
-         :sym->chan sym->chan})
+        {:par true :ch ch :bs [ch cdef]
+         :form (await-chan ch) :s2c s2c})
       {:par false
-       :sym->chan sym->chan
+       :s2c s2c
        :form (build-let forms bs)})))
 
-(defn parallelize
-  "Returns map
-    :form      Parallelized form that can be substituted.
-    :bs        Bindings that defines the channel
-    :ch        Channel to be dereferenced
-    :par       parallel?
-    :sym->chan Map of user symbols to channel symbols"
-  ([form] (parallelize form {} "g"))
-  ([form sym->chan ch-prefix]
-   (println "parallelizey" form sym->chan (when (seq? form) (function? (first form))))
-   (let [ret (cond (seq? form)
-         (if-let [f (first form)]
-           (cond (= (resolve 'let) (resolve  f))      (parallelize-let form sym->chan ch-prefix)
-                 (= (resolve 'map) (resolve f))      (parallelize-map form sym->chan "map")
-                 (or (function? f) (goop-fn f))        (parallelize-function-call form sym->chan ch-prefix)
-                 :else           (parallelize-coll form sym->chan ch-prefix))
-           '())
-         (coll? form)
-         (parallelize-coll form sym->chan ch-prefix)
-         :else
-         (do
-           (if-let [ch (get sym->chan form)]
-             {:ch ch :form (await-chan ch) :sym->chan sym->chan :par true}
-             {:form form})))]
-     (println "Returning" ret) ret)
-   ))
-
-
-(comment
-  (time (count (qmap #(Math/cos %) (range 500000))))  ;; 1200 ms
-  (time (count (pmap #(Math/cos %) (range 500000))))  ;; 2100 ms
-  (time (count (map #(Math/cos %) (range 500000)))) ;; 125ms
-  )
 
 
 (defn adj [x i] (if x (+ x i) i))
@@ -219,7 +233,7 @@
 
 
 
-(defn test-quasar-fiber
+#_(defn test-quasar-fiber
   [n]
   (let [z (System/nanoTime)
         ps (doall (for [i (range n)] (q/spawn-fiber blah)))
@@ -287,3 +301,56 @@
         vs (map ( fn [v] (-> v (- z) (* 1.0e-9))) vs)]
     [(apply min vs) (apply max vs) (count vs) (count  ids)]
     ))
+
+
+(comment
+  (ns goop.core)
+  (gdefn foo [x] (+ x 100))
+  
+  (assert (= (:form (parallelize '(map foo [1 2 3])))
+             '(map-and-deref (.gfn foo) [1 2 3])))
+
+
+  (pprint (parallelize '(let [x (foo 3)] (inc x))))
+  #_{:par true,
+     :ch g29368,
+     :bs
+     [g29368
+      (co.paralleluniverse.pulsar.core/promise
+       (clojure.core/fn
+         []
+         (clojure.core/let
+             [x29365
+              ((.gfn foo) 3)
+              form-29366
+              (co.paralleluniverse.pulsar.core/promise
+               (clojure.core/fn [] (clojure.core/list @x29365)))
+              form-29367
+              (co.paralleluniverse.pulsar.core/promise
+               (clojure.core/fn [] (clojure.core/apply inc @form-29366)))]
+             @form-29367)))],
+     :form @g29368,
+     :s2c {x x29365}}
+
+  (pprint (parallelize '(foo 3)))
+  #_{:ch g29371, :form @g29371, :par true, :bs (g29371 ((.gfn foo) 3))}
+
+  (pprint (parallelize '(inc 3)))
+  #_{:form (inc 3)}
+
+  (pprint (parallelize '(inc (foo 3))))
+  #_{:ch g29379,
+     :form @g29379,
+     :par true,
+     :bs
+     (g29377
+      ((.gfn foo) 3)
+      g29378
+      (co.paralleluniverse.pulsar.core/promise
+       (clojure.core/fn [] (clojure.core/list @g29377)))
+      g29379
+      (co.paralleluniverse.pulsar.core/promise
+       (clojure.core/fn [] (clojure.core/apply inc @g29378))))}
+  )
+
+
