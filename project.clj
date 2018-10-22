c61332785529226e9ef35abf15dd1cc270986897
Peter Fraenkel <pnf@podsnap.com>
2018-10-22 15:09:04 -0400

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.cache "0.6.5"]
-                 [co.paralleluniverse/pulsar "0.7.6"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.cache "0.7.1"]
+                 [co.paralleluniverse/pulsar "0.7.9"]
                  [co.paralleluniverse/comsat-httpkit "0.7.0"]
-                 [org.clojure/core.async "0.2.374"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.async "0.4.474"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
                  ]
-  :java-agents [[co.paralleluniverse/quasar-core "0.7.6"]]
+  :java-agents [[co.paralleluniverse/quasar-core "0.7.9"]]
   :repl-options {:timeout 120000
                  }
 
